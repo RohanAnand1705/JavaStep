637c03ce0e7f984332bb869befa6d9dd9c68c9ec
RohanAnand1705 <anandrohan1705@gmail.com>
2026-03-03 14:33:45 +0530

    Added UC2 OOPS BANNER APP

---
+++ UC2.java
@@ -0,0 +1,42 @@
+public class UC2 {
+
+    public static void main(String[] args) {
+
+        String gap = "    ";
+
+        System.out.println("  *****  " + gap +
+                           "  *****  " + gap +
+                           "  ****** " + gap +
+                           "  ****** ");
+
+        System.out.println(" **     **" + gap +
+                           " **     **" + gap +
+                           " **     **" + gap +
+                           " **      ");
+
+        System.out.println("**       *" + gap +
+                           "**       *" + gap +
+                           "**      **" + gap +
+                           "**       ");
+
+        System.out.println("**       *" + gap +
+                           "**       *" + gap +
+                           "******   " + gap +
+                           " ******  ");
+
+        System.out.println("**       *" + gap +
+                           "**       *" + gap +
+                           "**       " + gap +
+                           "       **");
+
+        System.out.println(" **     **" + gap +
+                           " **     **" + gap +
+                           " **      " + gap +
+                           " **    **");
+
+        System.out.println("  *****  " + gap +
+                           "  *****  " + gap +
+                           " **      " + gap +
+                           "  ****** ");
+    }
+}
